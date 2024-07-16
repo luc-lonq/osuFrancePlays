1175300792529bf868b515f361c7ba5d4ec03127
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-07-16 15:48:06 +0200

    feat: navbar + creation of default view files

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">osu! France Plays</span>
+        </a>
+        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <x-navlink href="/" :active="request()->is('/')">Accueil</x-navlink>
+                <x-navlink href="/regions" :active="request()->is('regions')">Classements régionaux</x-navlink>
+                <x-navlink href="/sotw" :active="request()->is('sotw')" aria-disabled="true">Score of the week</x-navlink>
+            </ul>
+        </div>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/regions', function () {
+    return view('regions');
+});
+
+Route::get('/sotw', function () {
+    return view('sotw');
+});
