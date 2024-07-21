a278c19d3af60def579b2b3b4ae2ddccf2c7282b
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-07-21 12:35:46 +0200

    feat: jsp ig

--- resources/views/components/regions/ranking.blade.php
+++ resources/views/components/regions/ranking.blade.php
@@ -25,9 +25,10 @@
                     @for ($i = 0; $i < rand(8,12); $i++)
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <div class="w-8 h-2 my-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>                        </th>
+                                <div class="w-8 h-2 my-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                            </th>
                             <td class="px-6 py-4">
-                                <div class="w-32 h-2 my-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>                        </th>
+                                <div class="w-32 h-2 my-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                             </td>
                             <td class="px-6 py-4">
                                 <div class="w-16 h-2 my-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
