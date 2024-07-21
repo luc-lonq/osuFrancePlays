5b6a2cee1d792ef647df499f58e2f8bb67abffd6
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-07-21 12:37:12 +0200

    feat: affichage plus condensé

--- resources/views/components/regions/ranking.blade.php
+++ resources/views/components/regions/ranking.blade.php
@@ -69,19 +69,19 @@
                 <tbody>
                     @foreach($players as $key=>$player)
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <th scope="row" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{ $player['region_rank'] }}
                             </th>
-                            <td class="px-6 py-4">
+                            <td class="px-6 py-3">
                                 {{ $player['username'] }}
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-6 py-3">
                                 {{ $player['pp'] }}pp
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-6 py-3">
                                 {{ $player['country_rank'] }}
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-6 py-3">
                                 {{ $player['rank'] }}
                             </td>
                         </tr>
