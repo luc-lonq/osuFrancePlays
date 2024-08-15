3a8c86932e4a4332c10cf6bf68f8f4a15dfc38f0
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-08-15 16:46:59 +0200

    mb wsh j'avais oublié de changer quelle méthode utiliser pour le scheduler

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('app:update-players-data')->everyMinute();
+Schedule::command('app:update-players-data')->dailyAt('00:00');
