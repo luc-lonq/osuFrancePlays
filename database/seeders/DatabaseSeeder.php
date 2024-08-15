8dbea1d45f0ad038f7f54c72c994406723d5e637
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-08-15 11:58:42 +0200

    pour du test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RegionSeeder::class);
-        $this->call(PlayerSeeder::class);
-        $this->call(SotwSeeder::class);
+        //$this->call(PlayerSeeder::class);
+        //$this->call(SotwSeeder::class);
     }
 }
