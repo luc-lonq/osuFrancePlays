24330a67cb69fa3f1b9c237320ed44b9ac034028
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-09-11 08:50:53 +0200

    vite fait

--- resources/views/index/staff.blade.php
+++ resources/views/index/staff.blade.php
@@ -1,10 +1,12 @@
 <x-layout>
     <div class="text-medium dark:text-gray-200 dark:bg-gray-900 rounded-lg mb-20 mt-10">
-        <div class="grid grid-cols-4 gap-4">
+        <div class="grid grid-cols-5 gap-4">
             @foreach($staff as $user)
                 <figure class="flex flex-col items-center">
-                    <img class="h-auto max-w-full rounded-lg" src="https://a.ppy.sh/{{ $user->osu_id }}" alt="">
-                    <figcaption class="mt-2 text-center text-gray-800 dark:text-gray-200">{{ $user->username}}</figcaption>
+                    <div class="flex items-center justify-center h-40 w-full">
+                        <img class="max-h-full max-w-full my-auto rounded-lg" src="https://a.ppy.sh/{{ $user->osu_id }}" alt="">
+                    </div>
+                    <figcaption class="mt-2 text-center text-gray-800 dark:text-gray-200">{{ $user->username }}</figcaption>
                 </figure>
             @endforeach
         </div>
