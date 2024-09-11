564f45899d46ee50d2a27b87ddda67412e320b1b
Luc Lacotte <luc.lacotte@etudiant.univ-lr.fr>
2024-09-11 09:42:35 +0200

    my bad

--- resources/views/index/staff.blade.php
+++ resources/views/index/staff.blade.php
@@ -3,8 +3,8 @@
         <div class="grid grid-cols-5 gap-4">
             @foreach($staff as $user)
                 <figure class="flex flex-col items-center">
-                    <div class="flex items-center justify-center h-40 w-full">
-                        <img class="max-h-full max-w-full my-auto rounded-lg" src="https://a.ppy.sh/{{ $user->osu_id }}" alt="">
+                    <div class="flex items-center justify-center my-auto w-full">
+                        <img class="w-full my-auto rounded-lg" src="https://a.ppy.sh/{{ $user->osu_id }}" alt="">
                     </div>
                     <figcaption class="mt-2 text-center text-gray-800 dark:text-gray-200">{{ $user->username }}</figcaption>
                 </figure>
